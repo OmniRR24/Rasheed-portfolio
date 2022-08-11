69d6dbe30a7a71f233b1d117679b5e68997f2e30
AAAAA <AAAAA@DESKTOP-M0J6QD5.local>
2022-08-11 19:23:56 +0800

    contact form

--- main.py
+++ main.py
@@ -13,26 +13,27 @@ RECEIVER = os.environ.get('RECIPIENT')
 @app.route('/', methods=['GET', 'POST'])
 def home():
     sent = False
-    subject = None
-    message = None
+    # subject = None
+    # message = None
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
         subject = request.form.get('subject').upper()
         description = request.form.get('desc')
-        message = f"Email Address: {email}\n\n" \
-                  f"Name: {name.title()}\n\n" \
-                  f"Project Description: {description.title()}"
-    #     with SMTP('smtp.gmail.com') as connection:
-    #         connection.starttls()
-    #         connection.login(user=USER, password=PASSWORD)
-    #         connection.sendmail(from_addr=USER, to_addrs=RECEIVER, msg=f'Subject: {subject.upper()}\n\n'
-    #                                                                    f'<b>You have a new message!<b>'
-    #                                                                    f'Client-Name: {name.title()}\n'
-    #                                                                    f'Client-Email: {email}\n'
-    #                                                                    f'Description: {description}\n')
+        # message = f"Email Address: {email}\n\n" \
+        #           f"Name: {name.title()}\n\n" \
+        #           f"Project Description: {description.title()}"
+        with SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(user=USER, password=PASSWORD)
+            connection.sendmail(from_addr=USER, to_addrs=RECEIVER, msg=f'Subject: {subject.upper()}\n\n'
+                                                                       f'<b>You have a new message!<b>'
+                                                                       f'Client-Name: {name.title()}\n'
+                                                                       f'Client-Email: {email}\n'
+                                                                       f'Description: {description}\n')
         sent = True
-    return render_template('index.html', message_sent=sent, subject=subject, message=message)
+        # return render_template('index.html', message_sent=sent, subject=subject, message=message)
+    return render_template('index.html', message_sent=sent)
 
 
 if __name__ == '__main__':
